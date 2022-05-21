ce7ce936679ea521404b16df9302433bafbb81e2
andyyyho <andyho@ucsb.edu>
2022-05-21 10:11:55 -0700

    Basic ui layouts

--- CalorieCounter/app/src/main/java/edu/ucsb/cs/cs184/caloriecounter/ui/logcalories/LogCaloriesViewModel.kt
+++ CalorieCounter/app/src/main/java/edu/ucsb/cs/cs184/caloriecounter/ui/logcalories/LogCaloriesViewModel.kt
@@ -1,13 +1,13 @@
-package edu.ucsb.cs.cs184.caloriecounter.ui.home
+package edu.ucsb.cs.cs184.caloriecounter.ui.logcalories
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class LogCaloriesViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "Home Page"
+        value = "Log Calories Page"
     }
     val text: LiveData<String> = _text
 }
