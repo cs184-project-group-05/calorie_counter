c0733b8432f1147dc024fdb4b7158a75051b90ce
Cameron King <cameron.s.king@gmail.com>
2022-05-22 18:50:55 -0700

    added view model state saving to home fragment

--- CalorieCounter/app/src/main/java/edu/ucsb/cs/cs184/caloriecounter/ui/home/HomeFragment.kt
+++ CalorieCounter/app/src/main/java/edu/ucsb/cs/cs184/caloriecounter/ui/home/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
 import edu.ucsb.cs.cs184.caloriecounter.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -28,14 +29,75 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        // - - - - - - - - - - home page title text - - - - - - - - - -
+        val title: TextView = binding.textHome
+        title.text = homeViewModel.text.value
+
+        // - - - - - - - - - - name text input field - - - - - - - - - -
+        val nameInputView = binding.textInputLayout8
+        nameInputView.editText?.setText(homeViewModel.name.value)
+
+        // - - - - - - - - - - age text input field - - - - - - - - - -
+        val ageInputView = binding.ageInput
+        ageInputView.editText?.setText(homeViewModel.age.value)
+
+        // - - - - - - - - - - weight text input field - - - - - - - - - -
+        val weightInputView = binding.weightInput
+        weightInputView.editText?.setText(homeViewModel.weight.value)
+
+        // - - - - - - - - - - height text input field - - - - - - - - - -
+        val heightInputView = binding.heightInput
+        heightInputView.editText?.setText(homeViewModel.height.value)
+
+        // - - - - - - - - - - gender selection dropdown menu input field - - - - - - - - - -
+        val genderDropdown = binding.genderDropdown
+        genderDropdown.editText?.setText(homeViewModel.gender.value)
+        val genders = arrayOf("Male", "Female")
+        (genderDropdown.editText as? MaterialAutoCompleteTextView)?.setSimpleItems(genders)
+
+        // - - - - - - - - - - goal selection dropdown menu input field - - - - - - - - - -
+        val goalDropdown = binding.goalDropdown
+        goalDropdown.editText?.setText(homeViewModel.goal.value)
+        val goals = arrayOf("Bulk Up", "Lose Weight")
+        (goalDropdown.editText as? MaterialAutoCompleteTextView)?.setSimpleItems(goals)
+
         return root
     }
 
     override fun onDestroyView() {
+        val homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        // - - - - - - - - - - name text input field - - - - - - - - - -
+        val nameInputView = binding.textInputLayout8
+        homeViewModel.name.value = nameInputView.editText?.text.toString()
+        // TODO: when fragment is destroyed, update the name in local database
+
+        // - - - - - - - - - - age text input field - - - - - - - - - -
+        val ageInputView = binding.ageInput
+        homeViewModel.age.value = ageInputView.editText?.text.toString()
+        // TODO: when fragment is destroyed, update the age in local database
+
+        // - - - - - - - - - - weight text input field - - - - - - - - - -
+        val weightInputView = binding.weightInput
+        homeViewModel.weight.value = weightInputView.editText?.text.toString()
+        // TODO: when fragment is destroyed, update the weight in local database
+
+        // - - - - - - - - - - height text input field - - - - - - - - - -
+        val heightInputView = binding.heightInput
+        homeViewModel.height.value = heightInputView.editText?.text.toString()
+        // TODO: when fragment is destroyed, update the height in local database
+
+        // - - - - - - - - - - gender text input field - - - - - - - - - -
+        val genderDropdown = binding.genderDropdown
+        homeViewModel.gender.value = genderDropdown.editText?.text.toString()
+        // TODO: when fragment is destroyed, update the goal in local database
+
+        // - - - - - - - - - - goal text input field - - - - - - - - - -
+        val goalDropdown = binding.goalDropdown
+        homeViewModel.goal.value = goalDropdown.editText?.text.toString()
+        // TODO: when fragment is destroyed, update the goal in local database
+
         super.onDestroyView()
         _binding = null
     }
--- CalorieCounter/app/src/main/java/edu/ucsb/cs/cs184/caloriecounter/ui/home/HomeViewModel.kt
+++ CalorieCounter/app/src/main/java/edu/ucsb/cs/cs184/caloriecounter/ui/home/HomeViewModel.kt
@@ -1,6 +1,5 @@
 package edu.ucsb.cs.cs184.caloriecounter.ui.home
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
@@ -9,5 +8,36 @@ class HomeViewModel : ViewModel() {
     private val _text = MutableLiveData<String>().apply {
         value = "Home Page"
     }
-    val text: LiveData<String> = _text
+    val text: MutableLiveData<String> = _text
+
+    private val _name = MutableLiveData<String>().apply {
+        value = null
+    }
+    val name: MutableLiveData<String> = _name
+
+    private val _age = MutableLiveData<String>().apply {
+        value = null
+    }
+    val age: MutableLiveData<String> = _age
+
+    private val _weight = MutableLiveData<String>().apply {
+        value = null
+    }
+    val weight: MutableLiveData<String> = _weight
+
+    private val _height = MutableLiveData<String>().apply {
+        value = null
+    }
+    val height: MutableLiveData<String> = _height
+
+    private val _gender = MutableLiveData<String>().apply {
+        value = null
+    }
+    val gender: MutableLiveData<String> = _gender
+
+    private val _goal = MutableLiveData<String>().apply {
+        value = null
+    }
+    val goal: MutableLiveData<String> = _goal
+
 }
