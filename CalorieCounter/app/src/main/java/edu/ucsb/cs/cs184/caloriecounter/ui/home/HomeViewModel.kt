a935b17dac849c52db237af94a146196f75a800a
Cameron King <cameron.s.king@gmail.com>
2022-05-23 17:17:39 -0700

    add local database, data entered on home page is now persistent

--- CalorieCounter/app/src/main/java/edu/ucsb/cs/cs184/caloriecounter/ui/home/HomeViewModel.kt
+++ CalorieCounter/app/src/main/java/edu/ucsb/cs/cs184/caloriecounter/ui/home/HomeViewModel.kt
@@ -1,44 +1,49 @@
 package edu.ucsb.cs.cs184.caloriecounter.ui.home
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import edu.ucsb.cs.cs184.caloriecounter.PrefRepository
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel(application: Application): AndroidViewModel(application) {
 
     // - - - - - - - - - - member variables - - - - - - - - - -
 
+    // by lazy = one time initialization: we only want one local database to be created ever
+    private val prefRepository by lazy { PrefRepository(application) }
+
     private val _name = MutableLiveData<String>().apply {
-        value = null
+        value = prefRepository.getName()
     }
     val name: MutableLiveData<String> = _name
 
     private val _age = MutableLiveData<String>().apply {
-        value = null
+        value = prefRepository.getAge()
     }
     val age: MutableLiveData<String> = _age
 
     private val _weight = MutableLiveData<String>().apply {
-        value = null
+        value = prefRepository.getWeight()
     }
     val weight: MutableLiveData<String> = _weight
 
     private val _height = MutableLiveData<String>().apply {
-        value = null
+        value = prefRepository.getHeight()
     }
     val height: MutableLiveData<String> = _height
 
     private val _gender = MutableLiveData<String>().apply {
-        value = null
+        value = prefRepository.getGender()
     }
     val gender: MutableLiveData<String> = _gender
 
     private val _goal = MutableLiveData<String>().apply {
-        value = null
+        value = prefRepository.getGoal()
     }
     val goal: MutableLiveData<String> = _goal
 
     private val _streak = MutableLiveData<Int>().apply {
-        value = 0
+        value = prefRepository.getStreak()
     }
     val streak: MutableLiveData<Int> = _streak
 
@@ -74,7 +79,7 @@ class HomeViewModel : ViewModel() {
     // - - - - - - - - - - setters - - - - - - - - - -
 
     fun setName(name: String): String {
-        // TODO: update the name in local database
+        prefRepository.setName(name)
         this.name.value = name
         return name
     }
@@ -83,7 +88,7 @@ class HomeViewModel : ViewModel() {
         // returns null if the age is invalid, otherwise return the input
         // calls to setAge should check for null return value and handle appropriately
         if (this.isWholeNumber(age)) {
-            // TODO: update the age in local database
+            prefRepository.setAge(age)
             this.age.value = age
             return age
         }
@@ -96,7 +101,7 @@ class HomeViewModel : ViewModel() {
         // returns null if the weight is invalid, otherwise return the weight
         // calls to setWeight should check for null return value and handle appropriately
         if (this.isNumber(weight)) {
-            // TODO: update the weight in local database
+            prefRepository.setWeight(weight)
             this.weight.value = weight
             return weight
         }
@@ -109,7 +114,7 @@ class HomeViewModel : ViewModel() {
         // returns null if the height is invalid, otherwise return the height
         // calls to setHeight should check for null return value and handle appropriately
         if (this.isNumber(height)) {
-            // TODO: update the height in local database
+            prefRepository.setHeight(height)
             this.height.value = height
             return height
         }
@@ -119,13 +124,13 @@ class HomeViewModel : ViewModel() {
     }
 
     fun setGender(gender: String): String {
-        // TODO: update the gender in local database
+        prefRepository.setGender(gender)
         this.gender.value = gender
         return gender
     }
 
     fun setGoal(goal: String): String {
-        // TODO: update the goal in local database
+        prefRepository.setGoal(goal)
         this.goal.value = goal
         return goal
     }
