c4cdea4795dd009751227b483222d7a886589030
andyyyho <andyho@ucsb.edu>
2022-05-25 22:50:32 -0700

    Added check for user authentication

--- CalorieCounter/app/src/main/java/edu/ucsb/cs/cs184/caloriecounter/MainActivity.kt
+++ CalorieCounter/app/src/main/java/edu/ucsb/cs/cs184/caloriecounter/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.ucsb.cs.cs184.caloriecounter
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -7,14 +8,25 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.firebase.auth.FirebaseAuth
 import edu.ucsb.cs.cs184.caloriecounter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mAuth = FirebaseAuth.getInstance()
+        val user = mAuth.currentUser
+
+        // Check if user is authenticated, if not then go to login page
+        if (user == null) {
+            val signInIntent = Intent(this, SignIn::class.java)
+            startActivity(signInIntent)
+            finish()
+        }
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
