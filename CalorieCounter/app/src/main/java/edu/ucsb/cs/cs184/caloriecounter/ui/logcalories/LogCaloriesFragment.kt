9ccca4493a32cb77e57bc2d4ecf304dd0684f4d0
AaronHuanger <43830121+AaronHuanger@users.noreply.github.com>
2022-06-03 03:31:16 -0700

    AH - logcaloriesFragment and logCaloriesViewModel edited with appRepository instead of prefRepository. Able to build, but haven't tested yet

--- CalorieCounter/app/src/main/java/edu/ucsb/cs/cs184/caloriecounter/ui/logcalories/LogCaloriesFragment.kt
+++ CalorieCounter/app/src/main/java/edu/ucsb/cs/cs184/caloriecounter/ui/logcalories/LogCaloriesFragment.kt
@@ -9,61 +9,36 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.textfield.TextInputEditText
 import edu.ucsb.cs.cs184.caloriecounter.R
 import edu.ucsb.cs.cs184.caloriecounter.databinding.LogCaloriesFragmentBinding
+import edu.ucsb.cs.cs184.caloriecounter.ui.home.HomeViewModel
 
 class LogCaloriesFragment : Fragment() {
     private var _binding: LogCaloriesFragmentBinding? = null
     private val binding get() = _binding!!
+    private lateinit var logCaloriesViewModel: LogCaloriesViewModel
+
 
     @SuppressLint("SetTextI18n")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        val logCaloriesViewModel = ViewModelProvider(this)[LogCaloriesViewModel::class.java]
+        logCaloriesViewModel = ViewModelProvider(this)[LogCaloriesViewModel::class.java]
         _binding = LogCaloriesFragmentBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        logCaloriesViewModel.getCurUserMutableLiveData().observe(viewLifecycleOwner, Observer {
+            logCaloriesViewModel.updateModel(it)
+            // TODO: implement the updateUI function in this fragment as well.
+            updateUI()
+        })
         // - - - - - - - - - - Update Function - - - - - - - - - -
-        logCaloriesViewModel.update()
+        //logCaloriesViewModel.update()
+        // TODO: The above function should be useless because of the new updateModel function.
 
-        // - - - - - - - - - - Calorie Goal Text - - - - - - - - - -
-        val calGoalValue = logCaloriesViewModel.calGoal.value.toString()
-        var calGoal1 = getString(R.string.calGoal1_under)
-        if (logCaloriesViewModel.goalLoseWeight.value == 0)  // if lose weight == false
-            calGoal1 = getString(R.string.calGoal1_over)
-        binding.textCalGoal.text = "$calGoal1 $calGoalValue ${getString(R.string.calGoal2)}"
 
-        // - - - - - - - - Make UI changes according to Calorie Count  - - - - - - - - -
-        logCaloriesViewModel.calCount.observe(viewLifecycleOwner) {
-            // daily calorie count total text
-            binding.numCalDaily.text = "$it"
-
-            // indicate in UI when calorie goal is met
-            var goalMet = logCaloriesViewModel.calGoal.value!! >= it!!
-            if (logCaloriesViewModel.goalLoseWeight.value == 0) goalMet = !goalMet
-            if (goalMet) {
-                binding.numCalDaily.setTextColor(resources.getColor(R.color.green))
-            } else {
-                binding.numCalDaily.setTextColor(resources.getColor(R.color.red))
-            }
-            if (it == 0) {
-                binding.numCalDaily.setTextColor(resources.getColor(androidx.appcompat.R.color.material_grey_600))
-            }
-        }
-
-        // - - - - - - - - - - Draw Meal Inputs from Saved State - - - - - - - - - -
-        val numMealInputsInt = logCaloriesViewModel.numMealInputsCreated.value
-        if (numMealInputsInt==0) {  // to draw initial meal input
-            logCaloriesViewModel.addMealInputViewModel()
-        }
-        logCaloriesViewModel.calorieArray.value?.forEachIndexed { index, calorieValue ->
-            if (calorieValue != -1) {
-                if (index == 0) addFirstMealInput(logCaloriesViewModel, calorieValue)
-                else addNewMealInput(logCaloriesViewModel, index, calorieValue)
-            }
-        }
 
         // - - - - - - - - - - Add Meal Input on Button Click - - - - - - - - - -
         val addMealButton = binding.button
@@ -126,6 +101,46 @@ class LogCaloriesFragment : Fragment() {
         Log.d("savedValues calCount", logCaloriesViewModel.calCount.value.toString())
         Log.d("savedValues calorieArray", logCaloriesViewModel.calorieArray.value.toString())
     }
+
+    private fun updateUI(){
+        // - - - - - - - - - - Calorie Goal Text - - - - - - - - - -
+        val calGoalValue = logCaloriesViewModel.calGoal.value.toString()
+        var calGoal1 = getString(R.string.calGoal1_under)
+        if (logCaloriesViewModel.goalLoseWeight.value == 0)  // if lose weight == false
+            calGoal1 = getString(R.string.calGoal1_over)
+        binding.textCalGoal.text = "$calGoal1 $calGoalValue ${getString(R.string.calGoal2)}"
+
+        // - - - - - - - - Make UI changes according to Calorie Count  - - - - - - - - -
+        logCaloriesViewModel.calCount.observe(viewLifecycleOwner) {
+            // daily calorie count total text
+            binding.numCalDaily.text = "$it"
+
+            // indicate in UI when calorie goal is met
+            var goalMet = logCaloriesViewModel.calGoal.value!! >= it!!
+            if (logCaloriesViewModel.goalLoseWeight.value == 0) goalMet = !goalMet
+            if (goalMet) {
+                binding.numCalDaily.setTextColor(resources.getColor(R.color.green))
+            } else {
+                binding.numCalDaily.setTextColor(resources.getColor(R.color.red))
+            }
+            if (it == 0) {
+                binding.numCalDaily.setTextColor(resources.getColor(androidx.appcompat.R.color.material_grey_600))
+            }
+        }
+
+        // - - - - - - - - - - Draw Meal Inputs from Saved State - - - - - - - - - -
+        val numMealInputsInt = logCaloriesViewModel.numMealInputsCreated.value
+        if (numMealInputsInt==0) {  // to draw initial meal input
+            logCaloriesViewModel.addMealInputViewModel()
+        }
+        logCaloriesViewModel.calorieArray.value?.forEachIndexed { index, calorieValue ->
+            if (calorieValue != -1) {
+                if (index == 0) addFirstMealInput(logCaloriesViewModel, calorieValue)
+                else addNewMealInput(logCaloriesViewModel, index, calorieValue)
+            }
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
