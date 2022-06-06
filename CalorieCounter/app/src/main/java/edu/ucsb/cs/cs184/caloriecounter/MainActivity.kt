eb5c8296e99c6fba1ef3c92d98b3c4b2112b4fcc
Mathew Kramsch <mathewkramsch@ucsb.edu>
2022-06-05 18:33:54 -0700

    mk - updated ui
    - made number inputs only able to input numbers
    - added fire icon to streak
    - made slight color changes

--- CalorieCounter/app/src/main/java/edu/ucsb/cs/cs184/caloriecounter/MainActivity.kt
+++ CalorieCounter/app/src/main/java/edu/ucsb/cs/cs184/caloriecounter/MainActivity.kt
@@ -2,7 +2,6 @@ package edu.ucsb.cs.cs184.caloriecounter
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
